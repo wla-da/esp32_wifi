52423e35c993e7e8deb78ca3e4f0e2bf8e59699e
wla-da <wla-da@mail.ru>
2023-09-24 01:03:38 +0300

    Миграция на ESP-IDF v5.1.1

--- src/main.c
+++ src/main.c
@@ -5,7 +5,8 @@
 #include "soc/rtc.h"
 #include "nvs_flash.h"
 #include "esp_app_format.h"
-//#include "esp_app_desc.h" в esp-idf v5
+#include "esp_chip_info.h"
+#include "esp_app_desc.h" //в esp-idf v5
 
 #include "wifi.h"
 
@@ -15,31 +16,30 @@
 
 //вывод общей информации  о системе для отладки
 void print_chip_info() {
-    ESP_LOGI(TAG, "Free heap %d kb, minimum %d kb", esp_get_free_heap_size()>>10, esp_get_minimum_free_heap_size()>>10);
-    
+    ESP_LOGI(TAG, "ESP-IDF version [%s]", esp_get_idf_version());
+    ESP_LOGI(TAG, "Free heap %" PRIu32 " kb, minimum %" PRIu32 " kb", esp_get_free_heap_size()>>10, esp_get_minimum_free_heap_size()>>10);
+      
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
-    ESP_LOGI(TAG, "CPU core %d", chip_info.cores);
-    ESP_LOGI(TAG, "CPU revision %d", chip_info.revision);
+    ESP_LOGI(TAG, "CPU core %" PRIu8, chip_info.cores);
+    ESP_LOGI(TAG, "CPU revision %" PRIu16, chip_info.revision);
 
     //информация о версии приложения esp_app_get_description() или esp_app_get_elf_sha256()/esp_ota_get_partition_description()
     //в esp-idf v5
-    /*
-    esp_app_desc_t *app_desc = esp_app_get_description();
+    
+    const esp_app_desc_t *app_desc = esp_app_get_description();
     
     ESP_LOGI(TAG, "Project: %s", app_desc->project_name);
     ESP_LOGI(TAG, "App version: %s", app_desc->version);
     ESP_LOGI(TAG, "Compile date: %s %s", app_desc->date, app_desc->time);
-    */
+    
 
     //установить частоту работы CPU
     //void rtc_clk_cpu_freq_set_config(const rtc_cpu_freq_config_t* config);
     rtc_cpu_freq_config_t cpu_freq;
     rtc_clk_cpu_freq_get_config(&cpu_freq);
-    ESP_LOGI(TAG, "CPU freq=%d MHz, source freq=%d MHz, divider=%d", cpu_freq.freq_mhz, cpu_freq.source_freq_mhz, cpu_freq.div);
+    ESP_LOGI(TAG, "CPU freq=%" PRIu32 " MHz, source freq=%" PRIu32 " MHz, divider=%" PRIu32, cpu_freq.freq_mhz, cpu_freq.source_freq_mhz, cpu_freq.div);
     ESP_LOGI(TAG, "APB freq=%0.2f MHz", rtc_clk_apb_freq_get()/(1000.0*1000));
-    
-    ESP_LOGI(TAG, "ESP-IDF version [%s]", esp_get_idf_version());
 }
 
 
@@ -49,4 +49,6 @@ void app_main() {
     print_chip_info();
 
     wifi_scan(10);
+
+    //wifi_init_sta();
 }
--- src/wifi.c
+++ src/wifi.c
@@ -171,7 +171,7 @@ static void print_cipher_type(int pairwise_cipher, int group_cipher)
 
 
 /*
-обработчик событий FiFi
+обработчик событий WiFi
 */
 static void event_handler(void* arg, esp_event_base_t event_base,
                                 int32_t event_id, void* event_data)
@@ -262,6 +262,8 @@ void wifi_scan(uint16_t max_ap) {
         }
         ESP_LOGI(TAG, "Channel \t%d", ap_info[i].primary);
     }
+
+    //esp_wifi_clear_ap_list();//очистим память после сканирования
     ESP_LOGI(TAG, "Scan Wi-Fi is complete!\n");
 }
 
@@ -274,13 +276,13 @@ void wifi_init_sta(void) {
 
 
 
-/*    s_wifi_event_group = xEventGroupCreate();
+    s_wifi_event_group = xEventGroupCreate();
 
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     esp_netif_create_default_wifi_sta();
 
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+/*    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
     esp_event_handler_instance_t instance_any_id;
